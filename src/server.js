8fc23e905a43fded0d7142d45a5a2bc9c243c991
duongdaoq <duongdaoq@gmail.com>
2023-10-30 20:57:35 +0700

    fix error connect db

--- src/server.js
+++ src/server.js
@@ -1,13 +1,13 @@
-import dotenv from 'dotenv';
-import app from './app.js';
-import mongoose from 'mongoose';
+import dotenv from "dotenv";
+import app from "./app.js";
+import mongoose from "mongoose";
 
 // set environment variable
-dotenv.config({ path: './src/config.env' });
+dotenv.config({ path: "./src/config.env" });
 
 // connect database
 const DB = process.env.DATABASE.replace(
-  '<password>',
+  "<password>",
   process.env.DATABASE_PASSWORD
 );
 
@@ -18,7 +18,7 @@ mongoose.connect(DB, {
 
 const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT, () => {
-  if (process.env.NODE_ENV === 'development') {
-    console.log('Listening on port ' + PORT + ' ❤️');
+  if (process.env.NODE_ENV === "development") {
+    console.log("Listening on port " + PORT + " ❤️");
   }
 });
