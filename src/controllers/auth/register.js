2186073ac03b9b50485747d4986b79e13ee978d1
HOROB1\lovea <nguyentheanh24102003@gmail.com>
2023-11-01 09:42:30 +0700

    update 1/11/2023

--- src/controllers/auth/register.js
+++ src/controllers/auth/register.js
@@ -1,11 +1,11 @@
 import User from "../../models/userModel.js";
-import catchAsync from "../../utils/catchAsync.js";
+import catchAsync from "../../utils/catchAsync";
 
 export const registerUser = catchAsync(async (req, res, next) => {
   const { username, firstName, lastName, email, password, passwordConfirm } =
     req.body;
 
-  const user = await User.create({
+  const user = new User({
     username,
     firstName,
     lastName,
@@ -13,6 +13,8 @@ export const registerUser = catchAsync(async (req, res, next) => {
     password,
     passwordConfirm,
   });
+
+  await user.save();
 
   res.status(201).json({
     status: "Registration Successful",
