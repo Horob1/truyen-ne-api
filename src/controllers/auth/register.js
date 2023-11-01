417e593cd864456c322c0d6a71828d9bab973186
HOROB1\lovea <nguyentheanh24102003@gmail.com>
2023-11-01 10:53:45 +0700

    fix register

--- src/controllers/auth/register.js
+++ src/controllers/auth/register.js
@@ -1,7 +1,7 @@
 import User from "../../models/userModel.js";
 import catchAsync from "../../utils/catchAsync";
 
-export const registerUser = catchAsync(async (req, res, next) => {
+const register = catchAsync(async (req, res, next) => {
   const { username, firstName, lastName, email, password, passwordConfirm } =
     req.body;
 
@@ -18,8 +18,5 @@ export const registerUser = catchAsync(async (req, res, next) => {
 
   res.status(201).json({
     status: "Registration Successful",
-    data: {
-      user,
-    },
   });
 });
