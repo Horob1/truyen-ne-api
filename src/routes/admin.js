75ee7441a835e2329f9965e392e8c83b79f869d1
duongdaoq <duongdaoq@gmail.com>
2023-11-11 18:04:08 +0700

    update folder controller

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,11 +1,11 @@
-import { Router } from "express";
-import { checkAdminRole } from "../middleware/checkAdminRole.js";
-import { listUser } from "../controllers/admin/listUser.js";
-import { checkJWT } from "../middleware/checkJWT.js";
-import { getUserInfo } from "../controllers/admin/getUserInfo.js";
+import { Router } from 'express';
+import { checkAdminRole } from '../middleware/checkAdminRole.js';
+import { listUser } from '../controllers/user.controller/admin/listUser.js';
+import { checkJWT } from '../middleware/checkJWT.js';
+import { getUserInfo } from '../controllers/user.controller/admin/getUserInfo.js';
 const router = Router();
 
-router.get("/list", checkJWT, checkAdminRole, listUser);
-router.get("/user-info/:id", checkJWT, checkAdminRole, getUserInfo);
+router.get('/list', checkJWT, checkAdminRole, listUser);
+router.get('/user-info/:id', checkJWT, checkAdminRole, getUserInfo);
 
 export default router;
