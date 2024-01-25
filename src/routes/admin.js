10bb4f593cbf05c56fd17bf45c778006960adce8
duongdaoq <duongdaoq@gmail.com>
2024-01-25 20:23:16 +0700

    resolve confl

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -3,9 +3,11 @@ import { checkAdminRole } from '../middleware/checkAdminRole.js';
 import { listUser } from '../controllers/user.controller/admin/listUser.js';
 import { checkJWT } from '../middleware/checkJWT.js';
 import { getUserInfo } from '../controllers/user.controller/admin/getUserInfo.js';
+import { deleteUser } from '../controllers/user.controller/admin/deleteUser.js';
 const router = Router();
 
 router.get('/list', checkJWT, checkAdminRole, listUser);
 router.get('/user-info/:id', checkJWT, checkAdminRole, getUserInfo);
+router.delete('/delete/:id', checkJWT, checkAdminRole, deleteUser);
 
 export default router;
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { checkJWT } from '../middleware/checkJWT.js';
 import { changePassword } from '../controllers/user.controller/client/changePassword.js';
+import { logOut } from '../controllers/user.controller/client/logOut.js';
 const router = Router();
 
 router.post('/change-password', checkJWT, changePassword);
+router.post('/logout', checkJWT, logOut);
 
 export default router;
