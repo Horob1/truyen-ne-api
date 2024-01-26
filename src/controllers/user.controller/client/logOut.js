049c13654be45a2620e1e068cf48b018aaffb22d
duongdaoq <duongdaoq@gmail.com>
2024-01-26 13:29:52 +0700

    new module profile user

--- src/controllers/user.controller/client/logOut.js
+++ src/controllers/user.controller/client/logOut.js
@@ -1,13 +1,13 @@
-// export const logOut = async (req, res) => {
-//   try {
-//     const { refreshToken } = req.cookies;
-//     if (!refreshToken) {
-//       return res.status(404).json('No refreshtoken found');
-//     }
-//     refreshTokens = refreshTokens.filter((token) => token !== refreshToken);
-//     res.clearCookie('refreshToken');
-//     res.status(200).json('Logout successful');
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// };
+export const logOut = async (req, res) => {
+  try {
+    const { refreshToken } = req.cookies;
+    if (!refreshToken) {
+      return res.status(404).json('No refreshtoken found');
+    }
+    refreshTokens = refreshTokens.filter((token) => token !== refreshToken);
+    res.clearCookie('refreshToken');
+    res.status(200).json('Logout successful');
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
