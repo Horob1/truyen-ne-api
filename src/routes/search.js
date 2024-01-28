d0bc975e860f71f4e13cfa97abb32a16b64676f7
duongdaoq <duongdaoq@gmail.com>
2024-01-28 10:39:29 +0700

    paginate, filter and limitfield

--- src/routes/search.js
+++ src/routes/search.js
@@ -1,11 +1,7 @@
 import { Router } from 'express';
-import { search } from '../controllers/novel/getNovelList.js';
 import { searchF } from '../controllers/forum/getForumPostList.js';
 
 const router = Router();
-
-router.route('/novel').get(search);
-
 router.route('/forum').get(searchF);
 
 export default router;
