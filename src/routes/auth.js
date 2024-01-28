b08ec4a4f730558703deb30b3fa683fa21eb2650
duongdaoq <duongdaoq@gmail.com>
2024-01-28 21:26:44 +0700

    new module refreshToken

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { loginUser } from '../controllers/user.controller/auth/login.js';
 import { register } from '../controllers/user.controller/auth/register.js';
+import { requestRefreshToken } from '../utils/requestRefreshToken.js';
 
 const router = Router();
 
 router.post('/register', register);
 router.post('/login', loginUser);
+router.post('/refresh-token', requestRefreshToken);
 
 export default router;
