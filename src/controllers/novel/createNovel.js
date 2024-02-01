b736fbc9bf19ce42bb8ae62075238bd84a5d4c0b
duongdaoq <duongdaoq@gmail.com>
2024-02-01 19:53:51 +0700

    add upload to createNovel

--- src/controllers/novel/createNovel.js
+++ src/controllers/novel/createNovel.js
@@ -7,7 +7,6 @@ export const createNovel = async (req, res) => {
 
     const translator = req.user.id;
 
-    // Tạo mới đối tượng Novel
     const newNovel = new Novel({
       name,
       description,
@@ -17,29 +16,6 @@ export const createNovel = async (req, res) => {
       author,
     });
 
-    // Kiểm tra xem có file coverImg được upload không
-    if (req.files['coverImg']) {
-      const coverImg = await cloudinary.uploader.upload(
-        req.files['coverImg'][0].path,
-        {
-          folder: 'Data/coverImg',
-        }
-      );
-      newNovel.coverImg = coverImg.secure_url;
-    }
-
-    // Kiểm tra xem có file photo được upload không
-    if (req.files['photo']) {
-      const photo = await cloudinary.uploader.upload(
-        req.files['photo'][0].path,
-        {
-          folder: 'Data/photo',
-        }
-      );
-      newNovel.photo = photo.secure_url;
-    }
-
-    // Lưu đối tượng Novel vào cơ sở dữ liệu
     await newNovel.save();
 
     // Trả về kết quả thành công
