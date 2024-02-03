70f5a4c3d6a5d32fa0fd9bc2a159d23e1cbf53f3
Horob1 <nguyentheanh24102003@gmail.com>
2024-02-03 23:11:42 +0700

    up permission for admin

--- src/controllers/user.controller/admin/listUser.js
+++ src/controllers/user.controller/admin/listUser.js
@@ -1,8 +1,11 @@
 import User from '../../../models/userModel.js';
+import APIFeatures from '../../../utils/apiFeatures.js';
 
 export const listUser = async (req, res, next) => {
   try {
-    const users = await User.find();
+    const features = new APIFeatures(User.find(), req.query);
+    features.filter().paginate().sort().limitFields();
+    const users = await features.data;
     res.status(200).json(users);
   } catch (err) {
     res.status(500).json(err);
--- src/routes/admin.js
+++ src/routes/admin.js
@@ -4,10 +4,12 @@ import { listUser } from '../controllers/user.controller/admin/listUser.js';
 import { checkJWT } from '../middleware/checkJWT.js';
 import { getUserInfo } from '../controllers/user.controller/admin/getUserInfo.js';
 import { deleteUser } from '../controllers/user.controller/admin/deleteUser.js';
+import { updateUser } from '../controllers/user.controller/admin/updateUser.js';
 const router = Router();
 
 router.get('/list', checkJWT, checkAdminRole, listUser);
 router.get('/user-info/:id', checkJWT, checkAdminRole, getUserInfo);
 router.delete('/delete/:id', checkJWT, checkAdminRole, deleteUser);
+router.update('/update/:id', checkJWT, checkAdminRole, updateUser);
 
 export default router;
